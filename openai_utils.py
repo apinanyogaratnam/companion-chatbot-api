4904b98d5f031eacdac79d6f40ac24a2cad49a49
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-10-16 13:44:19 -0400

    formatted code

--- openai_utils.py
+++ openai_utils.py
@@ -11,14 +11,14 @@ def get_response(input):
     restart_sequence = "\nHuman: "
 
     response = openai.Completion.create(
-    engine="davinci",
-    prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: {}".format(input),
-    temperature=0.9,
-    max_tokens=150,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0.6,
-    stop=["\n", " Human:", " AI:"]
+        engine="davinci",
+        prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: {}".format(input),
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        stop=["\n", " Human:", " AI:"]
     )
 
 
